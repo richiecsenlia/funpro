ac42317d92b230cc5ad8d7e06509064bcb367e72
Paskalis Abhista Bagaskara Y <paskalis.abhista@ui.ac.id>
2022-11-29 13:31:37 +0700

    Create backend folder

---
+++ backend/.stack-work/dist/8a54c84f/build/autogen/Paths_todo.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_todo (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\bin"
+libdir     = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\lib\\x86_64-windows-ghc-9.2.4\\todo-0.1.0.0-GY5GlIFuIScJ2XPCvfOvej"
+dynlibdir  = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\lib\\x86_64-windows-ghc-9.2.4"
+datadir    = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\share\\x86_64-windows-ghc-9.2.4\\todo-0.1.0.0"
+libexecdir = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\libexec\\x86_64-windows-ghc-9.2.4\\todo-0.1.0.0"
+sysconfdir = "C:\\Users\\user\\OneDrive\\Universitas Indonesia\\Semester 5\\PemFung\\funpro\\backend\\.stack-work\\install\\4d776be5\\etc"
+
+getBinDir     = catchIO (getEnv "todo_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "todo_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "todo_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "todo_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "todo_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "todo_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '\\'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/' || c == '\\'
