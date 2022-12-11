e2ef192befee03ebf36c9cd77208f8c86e3a922c
joh_rakatn <johanes.raka@ui.ac.id>
2022-12-11 09:11:14 +0700

    add AuthContext to Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -16,41 +17,75 @@ import Search from './components/Search';
 import FilterExpense from './components/FilterExpense';
 import SignupForm from './components/Register'
 import LoginForm from './components/Login';
+
+// sumber : https://www.freecodecamp.org/news/state-management-with-react-hooks/
+export const AuthContext = React.createContext();
+const initialState = {
+  isAuthenticated: false,
+  username: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("username",action.payload.username);
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.username
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        username : null
+      };
+    default:
+      return state;
+  }
+};
 function App() {
   // const url = "https://funpro-production.up.railway.app"
   const url = "http://localhost:8080"
+  const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        
-          <Route index element={<Home />} />
-          <Route path="all-expense" element={<ListExpense />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="create-jadwal" element={<CreateJadwal />}/>
-          <Route path="list-jadwal" element={<ListJadwal />}/>
-          <Route path="notes" element={<Notes />} />
-          <Route path="notes/createnote" element={<CreateNote />} />
-          <Route path="create-expense" element={<CreateExpense />}/>
-          <Route path= "search-year" element = {<Search
-                                                link="/search-year/"
-                                                title="Search Year"/>}/>
-          <Route path= "search-month" element = {<Search
-                                                link="/search-month/"
-                                                title="Search Month"/>}/> 
-          <Route path="search-year/:id" element = {<FilterExpense
-                                                    link={url+"/filterexpenseyear/"}/>}/>
-          <Route path="search-month/:id" element = {<FilterExpense
-                                                    link={url+"/filterexpensemonth/"}/>}/>
-          <Route path="register" element={<SignupForm />}/>
-          <Route path="login" element={<LoginForm />}/>
-          <Route path="*" element={<NoPage />} />
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
           
+            <Route index element={<Home />} />
+            <Route path="all-expense" element={<ListExpense />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="create-jadwal" element={<CreateJadwal />}/>
+            <Route path="list-jadwal" element={<ListJadwal />}/>
+            <Route path="notes" element={<Notes />} />
+            <Route path="notes/createnote" element={<CreateNote />} />
+            <Route path="create-expense" element={<CreateExpense />}/>
+            <Route path= "search-year" element = {<Search
+                                                  link="/search-year/"
+                                                  title="Search Year"/>}/>
+            <Route path= "search-month" element = {<Search
+                                                  link="/search-month/"
+                                                  title="Search Month"/>}/> 
+            <Route path="search-year/:id" element = {<FilterExpense
+                                                      link={url+"/filterexpenseyear/"}/>}/>
+            <Route path="search-month/:id" element = {<FilterExpense
+                                                      link={url+"/filterexpensemonth/"}/>}/>
+            {!state.isAuthenticated ? <Route path="register" element={<SignupForm />}/>: <Route path="login" element={<LoginForm />}/>}
+            <Route path="*" element={<NoPage />} />
+            
 
-        
-        
-      </Routes>
-    </BrowserRouter>
+          
+          
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
