8d26704bab45bdaaa1c721507d1d2038c7781a1a
Paskalis Abhista Bagaskara Y <paskalis.abhista@ui.ac.id>
2022-12-11 13:07:38 +0700

    Implementasi fitur jadwal untuk tiap user

--- frontend/src/components/ListJadwal.js
+++ frontend/src/components/ListJadwal.js
@@ -3,24 +3,29 @@ import JadwalCard from "./JadwalCard"
 import axios from "axios"
 import React from 'react'
 import { Card, Button} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 function ListJadwal(){
 
+	const navigate = useNavigate()
 	const [list, setList] = useState([])
+	const [feedback, setFeedback] = useState(false)
 
 	useEffect(() => {
-		axios.get('https://funpro-production-28fa.up.railway.app/jadwal')
+		axios.get('http://funpro-production-28fa.up.railway.app/jadwal/' + localStorage.getItem('username'))
 		.then(res => {
 			console.log(res.data)
 			setList(res.data)
+			setFeedback(false)
 		})
 	}, []);
 
 	const handleDelete = (id) => {
-		axios.delete('https://funpro-production-28fa.up.railway.app/delete-jadwal/' + id)
+		axios.delete('http://funpro-production-28fa.up.railway.app/delete-jadwal/' + id)
 		.then(res => {
 			console.log(res.data)
-			window.location.reload();
+			setFeedback(true)
+			sleep(1000)
 		})
 	}
 
@@ -39,13 +44,33 @@ function ListJadwal(){
 		)
 	})
 
+	const sleep = ms => new Promise(
+		resolve => setTimeout(resolve, ms)
+	  );
+
+	if(localStorage.getItem('username') === null){
+		return(
+		<div style={{ margin: '1rem'}}>
+			<p>Login Required</p>
+		</div>
+		)
+	}
+
+	if(list.length == 0){
+		return(
+		<div style={{ margin: '1rem'}}>
+			<p>Belum Ada Jadwal</p>
+		</div>
+		)
+	}
+
 	return(		
 		<div style={{ margin: '1rem'}}>
-			<p>List Jadwal</p>
 			<div>
+				<p>List Jadwal</p>
 				{cards}
+				{feedback?<p>Delete Success, please reload this page!</p>:""}
 			</div>
-			
 		</div>
 	)
 }
